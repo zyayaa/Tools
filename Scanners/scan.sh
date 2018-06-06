5fc4e9000c50159386599813cf75dcf400eb92d4
Joao Delgado <a9116@alunos.ipca.pt>
2018-06-06 16:35:51 +0100

    Change scans output

--- Scanners/scan.sh
+++ Scanners/scan.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nmap -sC -sV -sS -A -T4 -p- -v --open --reason --webxml -oX $1 $2
+mkdir nmap
+path=nmap/$1
+nmap -sC -sV -sS -A -T4 -p- -v --open --reason -oA $path $2
+xsltproc $path.xml -o $path.html
