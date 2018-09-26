b81c18473bba2eadb5d27e6819de6b4b86462b19
Joao Delgado <a9116@alunos.ipca.pt>
2018-09-26 12:48:30 +0100

    Reduce udp scan to Top ports

--- Scanners/udp.sh
+++ Scanners/udp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir nmap
 path=nmap/$1
-nmap -sU -sC -sV -A -T4 -p- -v --open --reason -oA $path $2
+nmap -sU -sC -sV -A -T4 -v --open --reason -oA $path $2
 xsltproc $path.xml -o $path.html
