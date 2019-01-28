d22b645db8ae9314c35d6b37b92187f97b93531b
Joao Delgado <jfpd56@gmail.com>
2019-01-28 22:50:12 +0000

    Added Ping Not scan

---
+++ Scanners/fullTcp-NoPing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir nmap
+path=nmap/$1-top10000-Pn
+nmap -Pn -n -sC -sV -sS -A -T4 -v --open --reason -oA $path $2
+xsltproc $path.xml -o $path.html
+path=nmap/$1-full-Pn
+nmap -Pn -n -sC -sV -sS -A -T4 -p- -v --open --reason -oA $path $2
+xsltproc $path.xml -o $path.html
