3037ffd8030d940b9251f2d74eb8e476cdd80395
Joao Delgado <jfpd56@gmail.com>
2022-04-02 23:58:39 +0100

    WIP

--- Scanners/fullTcp-NoPing.sh
+++ Scanners/fullTcp-NoPing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir nmap
-path=nmap/tcp_$1-top10000-Pn
+mkdir -p nmap
+path=nmap/tcp_$1-top1000-Pn
 nmap -Pn -sV -sS -A -T4 -v --open --reason -oA $path $2
 xsltproc $path.xml -o $path.html
 path=nmap/tcp_$1-full-Pn
